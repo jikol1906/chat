321eb13a3b929b8fc85366d4ee82a3f88556d19b
Boris Grunwald <borisgrunwald@gmail.com>
2019-01-20 20:51:58 +0100

    initial

---
+++ index.js
@@ -0,0 +1,56 @@
+
+const jsonReader = require('jsonfile');
+const express = require('express');
+const socket = require('socket.io');
+const fs = require('fs');
+
+const app = express();
+
+const server = app.listen(4000,() => {
+    'listening on port 4000'
+});
+
+app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection',(socket) => {
+
+    socket.on('chat', (data) => {
+
+        jsonReader.readFile(__dirname + '/messages.json')
+            .then(({lastMessages}) => {
+
+                console.log(lastMessages.length);
+
+                if(lastMessages.length >= 20) {
+                    lastMessages.shift();
+                }
+
+                lastMessages.push(data);
+
+                jsonReader.writeFile(__dirname + '/messages.json',{lastMessages})
+                    .then(res => console.log('complete'))
+                    .catch(error => console.log(error))
+            })
+            .catch(error => console.log(error));
+
+
+        io.sockets.emit('chat',data)
+    });
+
+
+    socket.on('typing',(data) => {
+        socket.broadcast.emit('typing',data)
+    })
+
+    socket.on('init',() => {
+
+        jsonReader.readFile(__dirname + '/messages.json')
+            .then(lastMessages => socket.emit('init',lastMessages))
+            .catch(err =>  console.log(err))
+
+    })
+
+
+});
---
+++ public/chat.js
@@ -0,0 +1,38 @@
+//Make connection
+
+const socket = io().emit('init');
+
+socket.on('init',(data) => {
+    data.lastMessages.forEach(
+        ({message,handle}) => output.innerHTML += '<p><strong>'+handle+':</strong>'+message+'</p>')
+
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+})
+
+const message = document.getElementById('message');
+const chatWindow = document.getElementById('chat-window');
+const handle = document.getElementById('handle');
+const send = document.getElementById('send');
+const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
+
+send.addEventListener('click',() => {
+    socket.emit('chat',{
+        message:message.value,
+        handle:handle.value
+    })
+});
+
+message.addEventListener('keypress',() => {
+    socket.emit('typing',handle.value)
+});
+
+socket.on('chat',(data) => {
+    output.innerHTML += '<p><strong>'+data.handle+':</strong>'+data.message+'</p>';
+    feedback.innerHTML = "";
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+});
+
+socket.on('typing',(data) => {
+    feedback.innerHTML = '<p><em>'+data+' is typing a message...</em></p>'
+});
